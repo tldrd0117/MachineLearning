5d6b181733daffd0ca8599cece65a8129040ce48
tldrd0117 <tldrd0117@gmail.com>
2017-07-09 21:47:28 +0900

    37page

--- 001-FirstProject/src/lsj/t2_kNN/kNN.py
+++ 001-FirstProject/src/lsj/t2_kNN/kNN.py
@@ -2,6 +2,7 @@
 from numpy import *
 import operator
 
+
 # 데이터셋 생성
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0,0], [0, 0.1]])
@@ -29,5 +30,25 @@ def classify0(inX, dataSet, labels, k):
     #아이템 정렬
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
+# Numpy 구문 분석 코드에 텍스트 기록하기
+def file2matrix(filename):
+    fr = open(filename)
+    # 파일의 줄 수 구하기
+    numberOfLines = len( fr.readlines() )
+    # 반환하기 위한 NumPy 행렬 생성하기
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    fr = open(filename)
+    # 리스트의 구문 분석하기datingTestSet
+    index = 0;
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(float(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
 
 
--- 001-FirstProject/src/lsj/t2_kNN/t2_kNN.py
+++ 001-FirstProject/src/lsj/t2_kNN/t2_kNN.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 import kNN
+import matplotlib
+import matplotlib.pyplot as plt
+from numpy import array
+
 group, labels = kNN.createDataSet()
 print( group )
 print( labels )
 
 result = kNN.classify0([0,0], group, labels, 3)
 print( result )
+
+datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+print( datingDataMat )
+print( datingLabels )
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+# 주당 아이스크림 소비량 y축, 비디오 게임으로 보내는 시간의 비율 x축
+ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
+plt.show()
+
+
